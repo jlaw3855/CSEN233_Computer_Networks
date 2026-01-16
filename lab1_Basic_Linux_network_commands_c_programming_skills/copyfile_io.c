3320bf6f5915626420a17f03208da70de28839f4
jlaw3855 <87198663+jlaw3855@users.noreply.github.com>
2026-01-15 18:39:01 -0800

    Add files via upload
    Removed unused libraries and variables

--- lab1_Basic_Linux_network_commands_c_programming_skills/copyfile_io.c
+++ lab1_Basic_Linux_network_commands_c_programming_skills/copyfile_io.c
@@ -1,43 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-
-const char *get_filetype(const char *file_n) {
-	const char *dot = strrchr(file_n, '.');
-	// Check if there are no dots in the requested filename, or if the filename is just a dot
-	if (dot == file_n) return "";
-	if (dot) return dot+1;
-	else return "";
-}
 
 void copy_file(const char* src_f, const char* dest_f) {
 	FILE *src, *dst;
-	const char *file_ext_ptr;
-	char buff[2048], file_ext[3];
 	unsigned int file_t;
 	int character;
-	size_t read_b, buffer_size = sizeof(buff);
 	clock_t start, end;
 	double cpu_time_used;
 
 	// Start the clock
 	start = clock();
 
-	// Check file extension to determine filetype
-	file_ext_ptr = get_filetype(src_f);
-	strcpy(file_ext, file_ext_ptr);
-	if (file_ext == "txt") file_t = 0;
-	if (file_ext == "bin") file_t = 1;
 
-	// Open Source File in r or rb mode based on filetype
+	// Open Source File in rrb mode
 	src = fopen(src_f, "rb");
 	if (!src) {
 		perror("Error opening source file");
 		exit(EXIT_FAILURE);
 	}
 
-	// Open Destination File in w or wb mode based on filetype
+	// Open Destination File in wb mode
 	dst = fopen(dest_f, "wb");
 	if (!dst) {
 		perror("Error opening destination file");
